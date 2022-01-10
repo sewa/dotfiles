97e9b4c737b41d51553f4f99f30f1f65c32a58bc
Severin Schwanck <schwanck@promptus-partners.de>
2022-01-10 09:48:27 +0100

    removed unused file

--- git-init.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "Severin Schwanck"
-git config --global user.email schwanck@promptus-partners.de
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
