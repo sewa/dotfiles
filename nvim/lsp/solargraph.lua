92538f70c4c7eb3576df27134d49a939fa4937d7
Severin Schwanck <s@schwanck.com>
2025-05-16 15:36:16 +0200

    fixed lsp

---
+++ nvim/lsp/solargraph.lua
@@ -0,0 +1,14 @@
+return {
+    cmd = { "bundle exec solargraph" },
+    settings = {
+        solargraph = {
+            autoformat = true,
+            completion = true,
+            diagnostic = true,
+            folding = true,
+            references = true,
+            rename = true,
+            symbols = true
+        }
+    }
+}
